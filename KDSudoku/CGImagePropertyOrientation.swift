4ebb1b8412144a39c2bc7c86543bc7d15ab11348
Klaus Dresbach <kd@klausdresbach.de>
2019-03-18 20:21:09 +0100

    190318 Starter Project with added mlmodel

---
+++ KDSudoku/CGImagePropertyOrientation.swift
@@ -0,0 +1,36 @@
+//
+//  CGImagePropertyOrientation.swift
+//  KDSudoku
+//
+//  Created by Klaus Dresbach on 18.03.19.
+//  Copyright © 2019 Klaus Dresbach. All rights reserved.
+//
+
+import UIKit
+
+extension CGImagePropertyOrientation {
+  init(_ orientation: UIImage.Orientation) {
+    switch orientation {
+    case .up: self = .up
+    case .upMirrored: self = .upMirrored
+    case .down: self = .down
+    case .downMirrored: self = .downMirrored
+    case .left: self = .left
+    case .leftMirrored: self = .leftMirrored
+    case .right: self = .right
+    case .rightMirrored: self = .rightMirrored
+    }
+  }
+}
+
+extension CGImagePropertyOrientation {
+  init(_ orientation: UIDeviceOrientation) {
+    switch orientation {
+    case .portraitUpsideDown: self = .left
+    case .landscapeLeft: self = .up
+    case .landscapeRight: self = .down
+    default: self = .right
+    }
+  }
+}
+
--- KDSudoku/ViewController.swift
+++ KDSudoku/ViewController.swift
@@ -10,11 +10,96 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet var imageView: UIImageView!
+  @IBOutlet var cameraButton: UIButton!
+  @IBOutlet var photoLibraryButton: UIButton!
+  @IBOutlet var resultsView: UIView!
+  @IBOutlet var resultsLabel: UILabel!
+  @IBOutlet var resultsConstraint: NSLayoutConstraint!
+  
+  var firstTime = true
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    resultsView.alpha = 0
+    resultsLabel.text = "choose or take a photo"
   }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    // Show the "choose or take a photo" hint when the app is opened.
+    if firstTime {
+      showResultsView(delay: 0.5)
+      firstTime = false
+    }
+  }
+  
+  @IBAction func takePicture() {
+    presentPhotoPicker(sourceType: .camera)
+  }
+  
+  @IBAction func choosePhoto() {
+    presentPhotoPicker(sourceType: .photoLibrary)
+  }
+  
+  func presentPhotoPicker(sourceType: UIImagePickerController.SourceType) {
+    let picker = UIImagePickerController()
+    picker.delegate = self
+    picker.sourceType = sourceType
+    present(picker, animated: true)
+    hideResultsView()
+  }
+  
+  func showResultsView(delay: TimeInterval = 0.1) {
+    resultsConstraint.constant = 100
+    view.layoutIfNeeded()
+    
+    UIView.animate(withDuration: 0.5,
+                   delay: delay,
+                   usingSpringWithDamping: 0.6,
+                   initialSpringVelocity: 0.6,
+                   options: .beginFromCurrentState,
+                   animations: {
+                    self.resultsView.alpha = 1
+                    self.resultsConstraint.constant = -10
+                    self.view.layoutIfNeeded()
+    },
+                   completion: nil)
+  }
+  
+  func hideResultsView() {
+    UIView.animate(withDuration: 0.3) {
+      self.resultsView.alpha = 0
+    }
+  }
+  
+  func classify(image: UIImage) {
+  }
+}
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    // Local variable inserted by Swift 4.2 migrator.
+    let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+    
+    picker.dismiss(animated: true)
+    
+    let image = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)] as! UIImage
+    imageView.image = image
+    
+    classify(image: image)
+  }
+}
 
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
+  return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+  return input.rawValue
 }
 
