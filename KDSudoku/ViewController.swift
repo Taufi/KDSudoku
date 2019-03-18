f8320cd6a6d6f36a00ac49e201b8c349f16d813a
Klaus Dresbach <kd@klausdresbach.de>
2019-03-18 19:42:20 +0100

    Initial Commit

---
+++ KDSudoku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KDSudoku
+//
+//  Created by Klaus Dresbach on 18.03.19.
+//  Copyright © 2019 Klaus Dresbach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
