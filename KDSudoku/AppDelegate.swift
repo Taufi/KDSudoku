fe39a6ec3ee453d35c70fca86847bd86d125c52b
Klaus Dresbach <kd@klausdresbach.de>
2019-04-28 21:00:01 +0200

    190428 EoB

--- KDSudoku/AppDelegate.swift
+++ KDSudoku/AppDelegate.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2019 Klaus Dresbach. All rights reserved.
 //
 //KD 190508 mlmodel von hier: https://github.com/patrykscheffler/sudoku-solver
+//KD 190428 - Video-Capturing abgekupfert aus CatsAndDogs
+// - dies wiederum aus Wenderlich - Machine Learning by Tutorials - Abschnitt „Classifying on live video“
 
 import UIKit
 
---
+++ KDSudoku/VideoCapture.swift
@@ -0,0 +1,37 @@
+//
+//  VideoCapture.swift
+//  KDSudoku
+//
+//  Created by Klaus Dresbach on 28.04.19.
+//  Copyright © 2019 Klaus Dresbach. All rights reserved.
+//
+
+import AVFoundation
+import UIKit
+
+public protocol VideoCaptureDelegate: class {
+  func videoCapture(_ capture: VideoCapture, didCaptureVideoFrame: CMSampleBuffer)
+}
+
+public class VideoCapture: NSObject {
+  public var previewLayer: AVCaptureVideoPreviewLayer?
+  public weak var delegate: VideoCaptureDelegate?
+  
+  public var frameInterval = 1
+  var seenFrames = 0
+  
+  let captureSession = AVCaptureSession()
+  let videoOutput = AVCaptureVideoDataOutput()
+  let queue = DispatchQueue(label: "de.klausdresbach.camera-queue")
+  
+  var lastTimestamp = CMTime()
+  
+  public func setUp(sessionPreset: AVCaptureSession.Preset = .medium, completion: @escaping (Bool)-> Void) {
+    queue.async {
+      let success = self.setUpCamera(sessionPreset: sessionPreset)
+      DispatchQueue.main.async {
+        completion(success)
+      }
+    }
+  }
+}
