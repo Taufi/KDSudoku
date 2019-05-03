1297b7901fa78a364c9e623adf55451658849960
Klaus Dresbach <kd@klausdresbach.de>
2019-05-03 17:01:57 +0200

    190503 VideoCapture Class finished - not yet tested

--- KDSudoku/VideoCapture.swift
+++ KDSudoku/VideoCapture.swift
@@ -26,12 +26,87 @@ public class VideoCapture: NSObject {
   
   var lastTimestamp = CMTime()
   
-//  public func setUp(sessionPreset: AVCaptureSession.Preset = .medium, completion: @escaping (Bool)-> Void) {
-//    queue.async {
-//      let success = self.setUpCamera(sessionPreset: sessionPreset)
-//      DispatchQueue.main.async {
-//        completion(success)
-//      }
-//    }
-//  }
+  public func setUp(sessionPreset: AVCaptureSession.Preset = .medium, completion: @escaping (Bool)-> Void) {
+    queue.async {
+      let success = self.setUpCamera(sessionPreset: sessionPreset)
+      DispatchQueue.main.async {
+        completion(success)
+      }
+    }
+  }
+  
+  func setUpCamera(sessionPreset: AVCaptureSession.Preset) -> Bool {
+    captureSession.beginConfiguration()
+    captureSession.sessionPreset = sessionPreset
+    
+    guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else {
+      print("Video nicht verfügbar")
+      return false
+    }
+  
+    guard let videoInput = try? AVCaptureDeviceInput(device: captureDevice) else {
+      print("AVCaptureDeviceInput lässt sich nicht bauen")
+      return false
+    }
+    
+    if captureSession.canAddInput(videoInput) {
+      captureSession.addInput(videoInput)
+    }
+    
+    let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+    previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
+    previewLayer.connection?.videoOrientation = .portrait
+    self.previewLayer = previewLayer
+  
+    let settings: [String : Any] = [kCVPixelBufferPixelFormatTypeKey as String: NSNumber(value: kCVPixelFormatType_32BGRA)]
+    
+    videoOutput.videoSettings = settings
+    videoOutput.alwaysDiscardsLateVideoFrames = true
+    videoOutput.setSampleBufferDelegate(self, queue:  queue)
+    
+    if captureSession.canAddOutput(videoOutput) {
+      captureSession.addOutput(videoOutput)
+    }
+    
+    // We want the buffers to be in portrait orientation otherwise they are
+    // rotated by 90 degrees. Need to set this _after_ addOutput()!
+    //KD 190502 kann ich das auch benutzen, um das blöde Umrechnen der Orientierung im ViewController zu verhindern?
+    videoOutput.connection(with: AVMediaType.video)?.videoOrientation = .portrait
+    
+    captureSession.commitConfiguration()
+    return true
+  }
+  
+  public func start() {
+    if !captureSession.isRunning {
+      seenFrames = 0
+      captureSession.startRunning()
+    }
+  }
+  
+  public func stop() {
+    if captureSession.isRunning {
+      captureSession.stopRunning()
+    }
+  }
+  
+}
+
+extension VideoCapture: AVCaptureVideoDataOutputSampleBufferDelegate {
+  
+  public func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+    
+    // Because lowering the capture device's FPS looks ugly in the preview,
+    // we capture at full speed but only call the delegate at its desired
+    // frame rate. If frameInterval is 1, we run at the full frame rate.
+    seenFrames += 1
+    if seenFrames >= frameInterval {
+      seenFrames = 0
+      delegate?.videoCapture(self, didCaptureVideoFrame: sampleBuffer)
+    }
+  }
+  
+  public func captureOutput(_ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+    print("dropped frame")
+  }
 }
