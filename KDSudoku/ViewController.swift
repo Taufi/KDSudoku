c5222bff45aa54665b60891e387c8fe8ea34169f
Klaus Dresbach <kd@klausdresbach.de>
2019-05-08 21:53:17 +0200

    190508 begin adding pathLayers

--- KDSudoku/ViewController.swift
+++ KDSudoku/ViewController.swift
@@ -30,6 +30,9 @@ class ViewController: UIViewController {
   let queue = DispatchQueue(label: "de.klausdresbach.digit-recognition-queue")
   let group = DispatchGroup()
   
+  //KD 190505 zur Visionalisierung des Sudoku-Rechtecks
+  var pathLayer: CALayer?
+  
   override func viewDidLoad() {
     super.viewDidLoad()
   
@@ -314,6 +317,62 @@ class ViewController: UIViewController {
       context.concatenate(transform)
       context.draw(cgImage, in: CGRect(x: 0, y: 0, width: width, height: height))
     }
+  }
+  
+  fileprivate func boundingBox(forRegionOfInterest: CGRect, withinImageBounds bounds: CGRect) -> CGRect {
+    
+    let imageWidth = bounds.width
+    let imageHeight = bounds.height
+    
+    // Begin with input rect.
+    var rect = forRegionOfInterest
+    
+    // Reposition origin.
+    rect.origin.x *= imageWidth
+    rect.origin.x += bounds.origin.x
+    rect.origin.y = (1 - rect.origin.y) * imageHeight + bounds.origin.y
+    
+    // Rescale normalized coordinates.
+    rect.size.width *= imageWidth
+    rect.size.height *= imageHeight
+    
+    return rect
+  }
+  
+  fileprivate func shapeLayer(color: UIColor, frame: CGRect) -> CAShapeLayer {
+    // Create a new layer.
+    let layer = CAShapeLayer()
+    
+    // Configure layer's appearance.
+    layer.fillColor = nil // No fill to show boxed object
+    layer.shadowOpacity = 0
+    layer.shadowRadius = 0
+    layer.borderWidth = 2
+    
+    // Vary the line color according to input.
+    layer.borderColor = color.cgColor
+    
+    // Locate the layer.
+    layer.anchorPoint = .zero
+    layer.frame = frame
+    layer.masksToBounds = true
+    
+    // Transform the layer to have same coordinate system as the imageView underneath it.
+    layer.transform = CATransform3DMakeScale(1, -1, 1)
+    
+    return layer
+  }
+  
+  fileprivate func draw(rectangles: [VNRectangleObservation], onImageWithBounds bounds: CGRect) {
+    CATransaction.begin()
+    for observation in rectangles {
+      let rectBox = boundingBox(forRegionOfInterest: observation.boundingBox, withinImageBounds: bounds)
+      let rectLayer = shapeLayer(color: .blue, frame: rectBox)
+      
+      // Add to pathLayer on top of image.
+      pathLayer?.addSublayer(rectLayer)
+    }
+    CATransaction.commit()
   }
   
   func presentAlert(_ title: String, error: NSError) {
