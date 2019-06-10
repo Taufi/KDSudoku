60092d95de88c811b37266294b9b70942f34abb6
Klaus Dresbach <kd@klausdresbach.de>
2019-06-10 12:17:25 +0200

    190610 many things work fine, addSolution not yet ;-)

--- KDSudoku/ViewController.swift
+++ KDSudoku/ViewController.swift
@@ -94,6 +94,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
   }
   
+  //KD 190610 Extrahiert die Ziffern aus dem Sudoku
   func classify(image: UIImage, completion: @escaping ((Int) -> Void)) {
     guard let ciImage = CIImage(image: image) else {
       print("Kann keine CIImage erzeugen")
@@ -118,6 +119,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
   }
   
+  //KD 190610 Verarbeitet das Ergebnis der gesuchten Ziffer: füllt in "completion" den Lösungsarray
   func processObservations(for request: VNRequest, completion: @escaping ((Int) -> Void), error: Error?) {
     DispatchQueue.main.async {
       if let results = request.results as? [VNClassificationObservation] {
@@ -138,12 +140,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
   }
   
+  //KD 190610 Verarbeitet das gesuchte Rechteck
   func handleDetectedRectangles(request: VNRequest?, error: Error?) {
     if let nsError = error as NSError? {
       self.presentAlert("Rectangle Detection Error", error: nsError)
       return
     }
     
+    //KD 190610 stellt fest, ob ein Rechteck gefunden wurde
     guard
       let results = request?.results as? [VNRectangleObservation],
       let rect = results.first,
@@ -180,6 +184,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 //      print("(\(uiImage.size.height),\(uiImage.size.width))")
 //    }
     
+    //KD 190610 zerlegt das Sudoku-Rechteck in die einzelnen Ziffernfelder und versucht die Ziffern zu
+    //          entschlüsseln
     for i in 0..<9 {
       for j in 0..<9 {
         let summand = i < 4 ? 0 : 1
@@ -191,17 +197,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
           
           //KD 190430 - das hatte ich vorher auf "DispatchQueue.global(qos: . userInitiated).async"
           // muss aber nicht sein, da dies eine callback-Funktion von VNDetectRectanglesRequest ist
+          //KD 190610 Entschlüsseln der Ziffern
           self.classify(image: uiImage) { (value) in
             self.sudokuMatrix[i][j] = value
           }
     
           //KD 190430 - das hatte ich vorher auf dem Main Thread (ist Quatsch) -> App hing dann,
           // wenn ich sie auf dem Device laufen ließ. Simulator und Photo Library ging.
-           self.saveImage(image: uiImage, imageName: "number\(i)\(j).png")
+//           self.saveImage(image: uiImage, imageName: "number\(i)\(j).png")
         }
       }
     }
     
+    //KD 190610 Wenn die Extrahierung aller 81 Ziffern abgeschlossen ist (daher die group-queue),
+    //          interpretiere ich das Ergebnis
     group.notify(queue: queue) { //KD 190502: siehe Erläuterung in AppDelegate
       DispatchQueue.main.async {
        
@@ -210,6 +219,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
           sudokoArray += self.sudokuMatrix[i].filter { $0 > 0}
         }
         
+        //KD 190610 Liegt ein vernünftiges Ergebnis vor? Falls weniger als 15 Ziffern, dann nicht.
         if sudokoArray.count > 15 {
           self.isReady = true
           print(self.sudokuMatrix)
@@ -225,22 +235,37 @@ class ViewController: UIViewController, ARSCNViewDelegate {
           self.pathLayer?.removeFromSuperlayer()
           self.pathLayer = nil
 //          self.showResultsView()
-          self.addBox()
+          self.addSolution(for: rect)
         }
       }
     }
   }
   
-  func addBox(x: Float = 0, y: Float = 0, z: Float = -0.2) {
-    let box = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0)
-    let boxNode = SCNNode()
-    boxNode.geometry = box
-    boxNode.position = SCNVector3(x, y, z)
+  //KD 190610 hier soll nun die Lösung der AR "auf das ungelöste Sudoku gelegt werden"
+  //KD 190610 funktioniert noch nicht
+  private func addSolution(for observedRect: VNRectangleObservation) {
     
-    sceneView.scene.rootNode.addChildNode(boxNode)
+    // Convert to 3D coordinates
+    guard let planeRectangle = PlaneRectangle(for: observedRect, in: sceneView) else {
+      print("No plane for this rectangle")
+      return
+    }
+    
+    let rectangleNode = RectangleNode(planeRectangle)
+    sceneView.scene.rootNode.addChildNode(rectangleNode)
   }
-
   
+//  func addBox(x: Float = 0, y: Float = 0, z: Float = -0.2) {
+//    let box = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0)
+//    let boxNode = SCNNode()
+//    boxNode.geometry = box
+//    boxNode.position = SCNVector3(x, y, z)
+//
+//    sceneView.scene.rootNode.addChildNode(boxNode)
+//  }
+
+
+  //KD 190610 gebe das Rect des auf dem Bildschirm sichtbaren Teil des Sudoku-Images zurück
   fileprivate func boundingBox(forRegionOfInterest: CGRect, withinImageBounds bounds: CGRect) -> CGRect {
     
     let imageWidth = bounds.width
@@ -264,6 +289,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     return rect
   }
   
+  //KD 190610 definiere den Layer des blauen Rahmens
   fileprivate func shapeLayer(color: UIColor, frame: CGRect) -> CAShapeLayer {
     // Create a new layer.
     let layer = CAShapeLayer()
@@ -283,12 +309,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     layer.masksToBounds = true
     
     // Transform the layer to have same coordinate system as the imageView underneath it.
-    //KD 190602 dreht den layer inmal um die x-Achse
+    //KD 190602 dreht den layer einmal um die x-Achse
     layer.transform = CATransform3DMakeScale(1, -1, 1)
     
     return layer
   }
   
+  //KD 190610 Zeichnet das blaue Rechteck un das gefundene Sudoku
   fileprivate func draw(rectangle: VNRectangleObservation, onImageWithBounds bounds: CGRect) {
     CATransaction.begin()
     
@@ -316,6 +343,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
   }
   
+  //KD 190610 sucht die Rechtecke auf dem übergebendem Foto
   func detectRectangles(uiImage: UIImage) {
     initArray()
     
@@ -357,6 +385,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
   }
   
+  //KD 190610 Debug-Hilfsfunktion, die Bilder abspeicher t
   func saveImage(image: UIImage, imageName: String){
     
     //create an instance of the FileManager
@@ -375,8 +404,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
 extension ViewController: ARSessionDelegate{
   
+  //KD 190610 hier greife ich die Bilder ab
   func session(_ session: ARSession, didUpdate frame: ARFrame) {
-    if count == 180 && !isReady {
+    if count == 240 && !isReady {
       count = 0
       let ciImage = CIImage(cvPixelBuffer: frame.capturedImage)
       let uiImage = convert(cmage: ciImage)
@@ -397,33 +427,34 @@ extension ViewController: ARSessionDelegate{
     let image:UIImage = UIImage.init(cgImage: cgImage)
     return image
   }
-  
-  //KD 190514 muss schauen, ob die Funktion geschachtelte Funktion bleiben soll
+
+  //KD 190610 bestimmt den Layer, der das Bild "Aspect Fill" darstellt. Heisst, es ragt links und rechts
+  //          heraus
   func preparePathLayer(originalImage: UIImage) {
     pathLayer?.removeFromSuperlayer()
     pathLayer = nil
-    
+
     // Transform image to fit screen.
     guard let cgImage = originalImage.cgImage else {
       print("Trying to show an image not backed by CGImage!")
       return
     }
-    
+
     let fullImageWidth = CGFloat(cgImage.width)
     let fullImageHeight = CGFloat(cgImage.height)
-    
+
     let imageFrame = sceneView.frame
     let widthRatio = fullImageWidth / imageFrame.width
     let heightRatio = fullImageHeight / imageFrame.height
-    
+
     // ScaleAspectFit: The image will be scaled down according to the stricter dimension.
     //KD 190530 der entscheidende Unterschied: iOS stellt das Photo auf dem Bildschirn im Format AspectFILL
     //          dar (vorher stand hier max(widthRatio, heightRatio))
     let scaleDownRatio = min(widthRatio, heightRatio)
-    
+
     let imageWidth = fullImageWidth / scaleDownRatio
     let imageHeight = fullImageHeight / scaleDownRatio
-    
+
     // Prepare pathLayer to hold Vision results.
     let xLayer = (imageFrame.width - imageWidth) / 2
     let yLayer = sceneView.frame.minY + (imageFrame.height - imageHeight) / 2
@@ -438,7 +469,8 @@ extension ViewController: ARSessionDelegate{
 
 }
 
-//KD 190525 von nier: https://stackoverflow.com/questions/40882487/how-to-rotate-image-in-swift
+//KD 190525 von hier: https://stackoverflow.com/questions/40882487/how-to-rotate-image-in-swift
+//KD 190610 dreht das Bild um 90°, da es in landscape-Format ankommt
 extension UIImage {
   func rotate(radians: CGFloat) -> UIImage {
     let rotatedSize = CGRect(origin: .zero, size: size)
@@ -457,7 +489,6 @@ extension UIImage {
       
       return rotatedImage ?? self
     }
-    
     return self
   }
 }
